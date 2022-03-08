f4b7da302135b19115656863f84f035c03a04c3a
gretazas <esminis@yahoo.com>
2022-03-08 17:30:27 +0000

    Add cards and their values, add 'you', and 'dealer'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-console.log("boo")
+let cards = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+let AceValue = {'0':[1, 11,]};
+let JQKValue = {'11': 10, '12': 10, '13':10};
+
+let you = {
+    'score' : 0,
+}
+
+let dealer = {
+    'score' : 0,
+}
