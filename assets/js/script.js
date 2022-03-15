596a84e413160cef0c5dfb2b3b395b5e717a4fa6
gretazas <esminis@yahoo.com>
2022-03-15 14:25:05 +0000

    Add innerHTML to button deal function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 let you = {
-    'score' : 0,
+    'score': 0,
 }
 
 let dealer = {
-    'score' : 0,
+    'score': 0,
 }
 
 /**Show random card 
@@ -12,116 +12,120 @@ let dealer = {
 function showCard() {
     const click = new Audio('assets/sounds/click.mp3');
     click.play();
-    let number = Math.floor(Math.random()* 13) + 1;
+    let number = Math.floor(Math.random() * 13) + 1;
     let image = document.createElement('img');
     image.src = 'assets/images/' + `${number}` + '.png';
     document.getElementById('you-main').appendChild(image);
-    cardValue(number); 
+
+    cardValue(number);
 }
 
 //Cards over 10 value = 10
 
 function cardValue(randomNumber) {
 
-        if(randomNumber > 10) {
-            randomNumber = 10;
-           showScore(randomNumber);
-        } else {
-            showScore(randomNumber);
-        }
+    if (randomNumber > 10) {
+        randomNumber = 10;
+        showScore(randomNumber);
+    } else {
+        showScore(randomNumber);
+    }
 }
 
 //Show players score
 
-function showScore(num) { 
+function showScore(num) {
 
-        if(num === 1) {
+    if (num === 1) {
 
-            let score = you.score + num;
+        let score = you.score + num;
 
-                if(score >= 21) {
+        if (score >= 21) {
 
-                                           
-                    document.getElementById('you-score').textContent = 'BUST!';
-                    document.getElementById('you-score').style.color = 'red';
-                    document.getElementById('message').textContent = "YOU LOST!!!";
-                    losses();
-                    document.getElementById('hit').disabled = true;
-                    document.getElementById('stand').disabled = true;   
 
-                } else if (score + 10 < 21) {
+            document.getElementById('you-score').textContent = 'BUST!';
+            document.getElementById('you-score').style.color = 'red';
+            document.getElementById('message').textContent = "YOU LOST!!!";
+            losses();
+            document.getElementById('hit').disabled = true;
+            document.getElementById('stand').disabled = true;
 
-                    score += 10;
-                    you.score = score ;   
-                    document.getElementById('you-score').textContent = score; 
-            console.log(score, '+10');  
-                } else {
+        } else if (score + 10 < 21) {
 
-                    score - 10;           
-                    you.score = score; 
-                    document.getElementById('you-score').textContent = score; console.log(score, '-10');
-                }            
-               
- 
+            score += 10;
+            you.score = score;
+            document.getElementById('you-score').textContent = score;
+            console.log(score, '+10');
+        } else {
+
+            score - 10;
+            you.score = score;
+            document.getElementById('you-score').textContent = score;
+            console.log(score, '-10');
+        }
+
+
+
+    } else {
+        let score = you.score + parseInt(`${num}`);
+        console.log(score);
+        if (score >= 21) {
+
+            console.log(score, '>=21');
+            document.getElementById('you-score').textContent = 'BUST!';
+            document.getElementById('you-score').style.color = 'red';
+            document.getElementById('message').textContent = "YOU LOST!!!";
+            losses();
+            document.getElementById('hit').disabled = true;
+            document.getElementById('stand').disabled = true;
 
         } else {
-                let score = you.score + parseInt( `${num}`) ; console.log(score) ;
-                if(score >= 21) {
 
-                                              console.log(score,'>=21');
-                    document.getElementById('you-score').textContent = 'BUST!';
-                    document.getElementById('you-score').style.color = 'red';
-                    document.getElementById('message').textContent = "YOU LOST!!!"; 
-                    losses();
-                    document.getElementById('hit').disabled = true;
-                    document.getElementById('stand').disabled = true;
+            you.score = score;
+            document.getElementById('you-score').textContent = score;
+            console.log(score, 'score')
 
-                } else {
 
-                you.score = score;    
-                document.getElementById('you-score').textContent = score;                            
-                          console.log( score, 'score')      
-                
-                
-                }
         }
+    }
 }
 
 /**Button 'Stand' changes active players from 'you' to 'dealer'*/
 
-function buttonStand (score) {
+function buttonStand(score) {
 
-    document.getElementById('hit').disabled = true;
-    document.getElementById('stand').disabled = true;
-    
+
+
     if (score > 17) {
 
-    winnerLoser();
+        winnerLoser();
 
     } else {
 
-    let number = Math.floor(Math.random()* 13) +1         ;
-    let image = document.createElement('img');
-    image.src = 'assets/images/' + `${number}` + '.png';
-    document.getElementById('dealer-main').appendChild(image);
-    
+        let number = Math.floor(Math.random() * 13) + 1;
+        let image = document.createElement('img');
+        image.src = 'assets/images/' + `${number}` + '.png';
+        document.getElementById('dealer-main').appendChild(image);
 
-    dealerCardValue(number);
-    
+
+        dealerCardValue(number);
+
     }
+    document.getElementById('hit').disabled = true;
+    document.getElementById('stand').disabled = true;
 }
 
 //Cards over 10 value = 10
 
 function dealerCardValue(randomNumber) {
-  
-    if(randomNumber > 10) {
+
+    if (randomNumber > 10) {
         randomNumber = 10;
         dealercards(randomNumber);
     } else {
         dealercards(randomNumber);
     }
-    
+
 }
 
 //Show dealers score                
@@ -129,60 +133,63 @@ function dealerCardValue(randomNumber) {
 function dealercards(num) {
 
 
-    if(num === 1) {
+    if (num === 1) {
 
-        let score = dealer.score + num; 
+        let score = dealer.score + num;
 
-                if(score >= 21) {
-console.log(' ACEdealerscore >= 21');
-                    document.getElementById('dealer-score').textContent = 'BUST!';
-                    document.getElementById('dealer-score').style.color = 'red';
-                    document.getElementById('message').textContent = "YOU WON!!!";
-                    wins();
-                    document.getElementById('hit').disabled = true;
-                    document.getElementById('stand').disabled = true;
+        if (score >= 21) {
+            console.log(' ACEdealerscore >= 21');
+            document.getElementById('dealer-score').textContent = 'BUST!';
+            document.getElementById('dealer-score').style.color = 'red';
+            document.getElementById('message').textContent = "YOU WON!!!";
+            wins();
+            document.getElementById('hit').disabled = true;
+            document.getElementById('stand').disabled = true;
 
-                } else if (score + 10 < 21) {
-console.log('ACEdealerscore += 10');
-                    score += 10
-                    dealer.score = score ;   
-                    document.getElementById('dealer-score').textContent = score; 
+        } else if (score + 10 < 21) {
+            console.log('ACEdealerscore += 10');
+            score += 10
+            dealer.score = score;
+            document.getElementById('dealer-score').textContent = score;
 
-                    buttonStand (score);
-            
-                } else {
-console.log('ACEdealerscore - 10;');
-                          
-                    dealer.score += score; 
-                    document.getElementById('dealer-score').textContent = score;
+            buttonStand(score);
 
-                    buttonStand (score);
-                            
-                }
+        } else {
+            console.log('ACEdealerscore - 10;');
+
+            dealer.score += score;
+            document.getElementById('dealer-score').textContent = score;
+
+            buttonStand(score);
+
+        }
 
 
     } else {
-        
-        let score = dealer.score + parseInt( `${num}`); console.log(dealer.score, 'dealer.score');console.log(num,'num');console.log(score,'score');
-                if(score >= 21) {
 
-console.log('dealerscore >= 21')                   
-                    document.getElementById('dealer-score').textContent = 'BUST!';
-                    document.getElementById('dealer-score').style.color = 'red';
-                    document.getElementById('message').textContent = "YOU WON!!!";
-                    wins();
-                    document.getElementById('hit').disabled = true;
-                    document.getElementById('stand').disabled = true;
-                } else {
-console.log('dealerscore')
-                    dealer.score = score;     
-                    document.getElementById('dealer-score').textContent = score;                            
-                                    
-                    buttonStand (score);
-                
-                }     
+        let score = dealer.score + parseInt(`${num}`);
+        console.log(dealer.score, 'dealer.score');
+        console.log(num, 'num');
+        console.log(score, 'score');
+        if (score >= 21) {
+
+            console.log('dealerscore >= 21')
+            document.getElementById('dealer-score').textContent = 'BUST!';
+            document.getElementById('dealer-score').style.color = 'red';
+            document.getElementById('message').textContent = "YOU WON!!!";
+            wins();
+            document.getElementById('hit').disabled = true;
+            document.getElementById('stand').disabled = true;
+        } else {
+            console.log('dealerscore')
+            dealer.score = score;
+            document.getElementById('dealer-score').textContent = score;
+
+            buttonStand(score);
+
+        }
     }
-    
+
 }
 
 //Decide who is the Winner
@@ -197,12 +204,14 @@ function winnerLoser() {
         document.getElementById('hit').disabled = true;
         document.getElementById('stand').disabled = true;
     } else if (you.score > dealer.score) {
-        document.getElementById('message').textContent = "YOU WON!!!"; console.log(you.score, "you", dealer.score, "dealer");
+        document.getElementById('message').textContent = "YOU WON!!!";
+        console.log(you.score, "you", dealer.score, "dealer");
         wins();
         document.getElementById('hit').disabled = true;
         document.getElementById('stand').disabled = true;
     } else if (you.score < dealer.score) {
-        document.getElementById('message').textContent = "YOU LOST!!!"; console.log(you.score, dealer.score);
+        document.getElementById('message').textContent = "YOU LOST!!!";
+        console.log(you.score, dealer.score);
         losses();
         document.getElementById('hit').disabled = true;
         document.getElementById('stand').disabled = true;
@@ -211,35 +220,40 @@ function winnerLoser() {
         document.getElementById('hit').disabled = true;
         document.getElementById('stand').disabled = true;
     }
-                                   
-} 
+
+}
 
 // Count wins and losses 
 
-function wins() {console.log('WIN');
+function wins() {
+    console.log('WIN');
     const harp = new Audio('assets/sounds/harp .wav');
     harp.play();
     let oldScore = parseInt(document.getElementById('win-score').textContent);
     document.getElementById('win-score').textContent = ++oldScore;
 }
 
-function losses() {console.log('LOSS');
-    
+function losses() {
+    console.log('LOSS');
+
     const glassBreaking = new Audio('assets/sounds/glassbreaking.wav');
     glassBreaking.play();
     let oldScore = parseInt(document.getElementById('loss-score').textContent);
     document.getElementById('loss-score').textContent = ++oldScore;
 }
 
- /**Start over */                          
+/**Start over */
 
 function buttonDeal() {
+
+
+
 
     document.getElementById('you-score').style.color = 'black';
     document.getElementById('dealer-score').style.color = 'black';
     const gongSound = new Audio('assets/sounds/gong.wav');
     gongSound.play();
-    
+
 
     document.getElementById('stand').disabled = false;
     document.getElementById('hit').disabled = false;
@@ -254,14 +268,14 @@ function buttonDeal() {
 
     document.getElementById('message').textContent = "GOOD LUCK!!!";
 
-    let images = document.getElementById('main').querySelectorAll('img');
-        for(let i=0; i<= images.length; i++) {
-            images[i].remove();
-        }
+    const cardContainers = ['#you-main', '#dealer-main'];
+    cardContainers.forEach(container => {
+        document.querySelector(container).innerHTML = '';
+    });
 
-       
+
 }
 
 document.getElementById('stand').addEventListener('click', buttonStand);
 document.getElementById('deal').addEventListener('click', buttonDeal);
-document.getElementById('hit').addEventListener('click', showCard); 
+document.getElementById('hit').addEventListener('click', showCard);
